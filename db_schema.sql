e69c790ac0a17e925de933f52d111e6c6b8f9b4d
Matt Long <beaver08@gmail.com>
2010-01-23 17:25:52 -0800

    alpha version working

---
+++ db_schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE Image;
+
+CREATE TABLE Image (
+ID integer NOT NULL AUTO_INCREMENT,
+OriginalFileName varchar(20) NULL,
+ReversedFileName varchar(20) NULL,
+PRIMARY KEY(ID)
+);
--- revgif.py
+++ revgif.py
@@ -1,14 +1,19 @@
 import web
+import os
 
 render = web.template.render('templates/')
 db = web.database(dbn='mysql', user='mateo', pw='b', db='revgifDB')
 
 urls = (
     '/', 'index',
-    '/add', 'add'
+    '/add', 'add',
+    '/(\d+)', 'show'
 )
 
 app = web.application(urls, globals())
+
+filedir = './images/original/'
+outdir = './images/reversed/'
 
 class index:
     def GET(self):
@@ -18,14 +23,35 @@ class index:
 class add:
     def POST(self):
         i = web.input(myfile={})
-        filedir = './images/original'
         gif = i['myfile'].file
-        if gif != None:
-            fout = open(filedir + '/' + 'test.gif', 'w')
-            fout.write(gif.read())
-            fout.close()
+        if gif != None:            
+            self.add_gif(gif)
 
-        #n = db.insert('Image', FileName=i.filename)
         raise web.seeother('/')
+
+    def add_gif(self, gif):
+        #TODO: validate that it's an animated gif
+
+        n = db.insert('Image',OriginalFileName='')
+        originalFN = str(n) + '.gif'
+        outFN = originalFN
+        fout = open(filedir + originalFN, 'w')
+        fout.write(gif.read())
+        fout.close()
+
+        toRun = 'gimp -ib \'(reverse-gif "' + filedir + originalFN + '" "' + outdir + outFN + '")\' -b \'(gimp-quit 0)\''
+        result = os.system(toRun)
+        db.query("UPDATE Image SET OriginalFileName=$ofn, ReversedFileName=$rfn WHERE ID = $id", vars=dict(ofn=originalFN,rfn=outFN,id=n))
+
+class show:
+    def GET(self,id):
+        res = db.select('Image', {'ID':id}, where="id = $ID")
+        for item in res:
+            web.header('Content-Type', 'image/gif')
+            #web.debug("RFN: " + item.ReversedFileName)
+            return open(outdir + item.ReversedFileName,'r').read()
+        raise web.seeother('/')
+        
+        
 
 if __name__ == "__main__": app.run()
